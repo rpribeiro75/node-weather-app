648c3aa8650424bff1dc2854402f4709560910a9
Rui Ribeiro <RuiRibeiro@Inobest.local>
2021-03-25 00:31:55 +0000

    correcao fetch

--- public/js/app.js
+++ public/js/app.js
@@ -19,7 +19,7 @@ weatherForm.addEventListener("submit",(e)=>{
     messageOne.textContent = "Loading..."
     messageTwo.textContent = ""
 
-    r("/weather?address="+location).then((response)=>{
+    fetch("/weather?address="+location).then((response)=>{
         response.json().then((data)=>{
             if(data.error){
                 messageOne.textContent = data.error
